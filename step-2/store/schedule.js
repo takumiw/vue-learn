cd2c4508d77367f12a6dede27e564d92d7356992
takumiw <w.tak.1229@gmail.com>
2020-02-06 20:04:15 +0900

    feat: calendar

---
+++ step-2/store/schedule.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+    scheduleList: [], // Array<{ id: Number, day: Number, text: String }>
+    lastScheduleId: 0,
+  })
+  
+  export const getters = {
+    getScheduleListByDay: (state) => (day) => {
+      return state.scheduleList.filter(schedule => schedule.day === day)
+    },
+  }
+  
+  export const mutations = {
+    addSchedule (state, { day, text }) {
+      state.scheduleList.push({
+        id: ++state.lastScheduleId,
+        day,
+        text,
+      })
+    },
+  }
